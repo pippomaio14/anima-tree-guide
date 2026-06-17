a70d819dd71c07bf296c49de28f450fc5a304b1f
pippomaio14 <pippo.maio14@gmail.com>
2026-06-17 10:18:05 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,13 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "app.lovable.6a98369d645a45a89d645f6f4467b581",
+  appId: "com.anima.treeguide",
   appName: "anima-tree-guide",
   webDir: "dist",
-  server: {
-    url: "https://6a98369d-645a-45a8-9d64-5f6f4467b581.lovableproject.com?forceHideBadge=true",
-    cleartext: true,
-  },
+  //server: {
+  // url: "https://6a98369d-645a-45a8-9d64-5f6f4467b581.lovableproject.com?forceHideBadge=true",
+  // cleartext: true,
+  //},
   plugins: {
     GoogleMaps: {
       apiKey: 'AIzaSyCLUtn_ue87Sn3D_VdpDQO6RaeH4tgzLIc', // <-- DEVE ESSERE LA STESSA
