f0e3aa2967dd4488075d1a2537ac130d64fb542e
pippomaio14 <pippo.maio14@gmail.com>
2026-06-17 09:36:29 +0200

    Add Google Maps plugin configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,6 +8,11 @@ const config: CapacitorConfig = {
     url: "https://6a98369d-645a-45a8-9d64-5f6f4467b581.lovableproject.com?forceHideBadge=true",
     cleartext: true,
   },
+  plugins: {
+    GoogleMaps: {
+      apiKey: 'AIzaSyCLUtn_ue87Sn3D_VdpDQO6RaeH4tgzLIc', // <-- DEVE ESSERE LA STESSA
+    },
+  },
 };
 
 export default config;
